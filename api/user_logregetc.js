e959e84d0c9758211e2346c6a79d2bc778c8793a
Purple1ce <zoellner-fabian@web.de>
2023-06-05 12:16:17 +0200

    test

--- api/user_logregetc.js
+++ api/user_logregetc.js
@@ -23,9 +23,13 @@ export async function login(req, res) {
 
 export async function register(req, res) {
 
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = req.body.user
+    const pw = req.body.password
+    const email = req.body.email
+
+    const hashedPw= await bcrypt.hash(req.body.password, 10);
     try {
-        await query("Insert into Account(Benutzer, Passwd, Email) values(?,?,?)", [req.body.user, hashedPassword, req.body.email]);
+        await query("Insert into Account(Benutzer, Passwd, Email) values(${user},${hashedPw},${email})", );
         res.status(200).send("Registration sucessfull");
     } catch (err) {
         res.status(500).send(err);
