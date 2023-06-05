664e114acf268a12baef7b417be85258e39c2ca4
Purple1ce <zoellner-fabian@web.de>
2023-06-05 09:06:41 +0200

    9

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const parser = require("body-parser");
 
-import { router } from "./api/routes.mjs";
+const router =require("./api/routes.js");
 
 
 
