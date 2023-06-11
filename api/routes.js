bff853b30c37f71515a73a325e1313510d829a7f
Purple1ce <zoellner-fabian@web.de>
2023-06-11 10:43:17 +0200

    Authentication Test

--- api/routes.js
+++ api/routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { checkRegistrationFields } from "./validation/register.js";
-import { login, register } from "./user_logregetc.js";
+import { login, register, authenticateUser } from "./user_logregetc.js";
 import { db } from "./database.js";
 
 
@@ -39,13 +39,30 @@ router.get("/modul", async (req, res) => {
         const id = req.param('id');
 
         const result = await db.pool.query("Select * from Modul where ModulId = ?", [id]);
-        res.status(200).send(result);
+        res.status(200).json(result);
 
     } catch (err) {
         res.status(400).send(err)
     }
 });
 
+router.get("/completed", async (req, res) => {
+    try {
+        const accountId = authenticateUser(req);
+
+        const query =   `SELECT M.Name
+                         FROM Modul M
+                         JOIN ModulAbgeschlossen MA ON M.ModulID = MA.ModulID
+                         WHERE MA.AccountId = ?`;
+
+        const result = await db.pool.query(query, [accountId]);
+
+        res.status(400).json(result);
+
+    } catch (err) {
+        res.status(400).send(err);
+    }
+})
 router.get("/timetable_data", async (req, res) => {
     try {
         const id = req.param('id');
--- api/user_logregetc.js
+++ api/user_logregetc.js
@@ -12,7 +12,8 @@ export async function login(req, res) {
         if (result.length > 0) {
             const hashedPassword = result[0].Passwd;
             if (await bcrypt.compare(password, hashedPassword)) {
-                token = generateAccessToken(user)
+                const token = await generateAccessToken(user)
+                res.status(201).header('Authorization', 'Bearer ' + token).json({ message: 'Login complete' });
                 res.status(200).json(token);
             } else {
                 res.status(401).send("Password incorrect!");
@@ -29,11 +30,11 @@ export async function register(req, res) {
     const pw = req.body.password
     const email = req.body.email
 
-    const hashedPw= await bcrypt.hash(req.body.password, 10);
+    const hashedPw= await bcrypt.hash(pw, 10);
     try {
         await db.pool.query("Insert into Account(Benutzer, Passwd, Email) values(?, ?, ?)", [user, hashedPw, email]);
-        token = generateAccessToken(user)
-        res.status(201).json(token);
+        const token = await generateAccessToken(user)
+        res.status(201).header('Authorization', 'Bearer ' + token).json({ message: 'Registration complete' });
     } catch (err) {
         res.status(500).send(err);
     }
@@ -42,17 +43,27 @@ export async function register(req, res) {
 async function generateAccessToken(user) {
     try {
         const token = jwt.sign({ user: user }, process.env.TOKEN_SECRET, { expiresIn: '1h' });
-        const result = await db.pool.query('Select AccountID from Account Where Benutzer = ?', [user]);
-        console.log(result);
+        const expirationTimestamp = new Date(Date.now() + (60 * 60 * 1000));
+        const result = await db.pool.query("Select AccountID from Account Where Benutzer = ?", [user]);
+        const accountID = result[0].AccountID
+        await db.pool.query('Insert Into AccessToken (Token, expiresAt, AccountID) Values (?, ?, ?)', [token, expirationTimestamp, accountID])
+        const currentDate = new Date();
+        const result = await db.pool.query('DELETE FROM AccessToken WHERE expiresAt <= ? AND AccountID = ? '[currentDate, accountID]);
         return token;
-    }
-    catch (err) {
-        throw err;
-    }
-    
-   
+    }  
 }
 
-export function authenticateUser(token) {
+export async function authenticateUser(req) {
+    const token = req.headers.authorization.split(' ')[1];
 
+    try {
+        const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+        const result = await db.pool.query('SELECT * FROM AccessToken WHERE Token = ?', [token])
+        if (result && result.length > 0) {
+            return result[0].AccountID
+        }
+        else {
+            throw new Error("Authentication failed");
+        }
+    }
 }
